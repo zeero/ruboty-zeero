5f0ca9240ae9427eb31296c242ce3b2384803843
zeero <zeero26@gmail.com>
2016-12-19 01:04:14 +0900

    Chronoでのcron処理の確認

--- lib/ruboty/handler/zeero-cron.rb
+++ lib/ruboty/handler/zeero-cron.rb
@@ -1,14 +1,15 @@
+require 'chrono'
+
 module Ruboty
   module Handlers
-    class Test < Base
-      on(
-        /test/i,
-        name: "test",
-        description: "Test!"
-      )
-
-      def test(message)
-        message.reply("Test")
+    class CronP2Q < Base
+      def initialize(robot)
+        super
+        Chrono::Trigger.new("10 * * * *") {
+          robot.say({
+            body: "ChronoTrigger!",
+          })
+        }.run
       end
     end
   end
