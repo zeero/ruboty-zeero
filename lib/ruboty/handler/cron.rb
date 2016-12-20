b6be89a6e3ea9b8f568afd1768884ee713b95983
zeero <zeero26@gmail.com>
2016-12-21 00:36:29 +0900

    CronP2Q実装

---
+++ lib/ruboty/handler/cron.rb
@@ -0,0 +1,117 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'date'
+require 'logger'
+
+require 'chrono'
+require 'qiita'
+
+module Ruboty
+  module Handlers
+    class CronP2Q < Base
+      @logger = Logger.new($stdout)
+
+      def initialize(robot)
+        super
+        @thread = Thread.new {
+          schedule = ! $DEBUG ? "0 0 * * *" : "* * * * *"
+          Chrono::Trigger.new(schedule) {
+            msg = Message.new(robot: robot)
+            attributes = {
+              to: "log@conference.zeero.xmpp.slack.com/zeero",
+              type: "groupchat",
+              body: p2q,
+              original: msg.original
+            }
+            robot.say(attributes)
+          }.run
+        }
+      end
+
+      def p2q
+        json = fetch_pocket
+        qids = filter_qiita(json)
+        msg = stock_qiita(qids)
+        return msg
+      end
+
+      def fetch_pocket
+        # header
+        headers = {
+          "Content-type" => "application/x-www-form-urlencoded; charset=utf8",
+        }
+
+        # body
+        body = URI.encode_www_form({
+          consumer_key: ENV["RUBOTY_POCKET_CONSUMER_KEY"],
+          access_token: ENV["RUBOTY_POCKET_TOKEN"],
+          since: (Date.today - 1).to_time.to_i,
+          search: 'qiita.com',
+          sort: 'oldest',
+          count: "1",
+        })
+
+        # post
+        uri = URI.parse("https://getpocket.com/v3/get")
+        begin
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.set_debug_output $stderr if $DEBUG
+          # SSL
+          http.use_ssl = true
+          # 接続タイムアウト
+          http.open_timeout = 5
+          # 読込タイムアウト
+          http.read_timeout = 10
+
+          # 実行
+          res = http.post(uri.path, body, headers)
+
+          # 結果取得
+          case res
+          when Net::HTTPSuccess
+            return JSON.parse(res.body)
+          when Net::HTTPRedirection
+            logger.warn "HTTP Redicect: code=#{res.code} message=#{res.message}"
+          else
+            logger.error "HTTP Error: code=#{res.code} message=#{res.message}"
+          end
+        rescue => e
+          logger.error e.message
+        end
+      end
+
+      def filter_qiita(json)
+        map = {}
+        json["list"].each do |pid, pitem|
+          given_url = pitem["given_url"]
+          qid = given_url ? given_url.match(/\/\/qiita\.com\/.+\/items\/(.+)$/) : nil
+          if qid
+            map[qid[1]] = pitem["given_title"]
+          end
+        end
+        return map
+      end
+
+      def stock_qiita(qids)
+        client = Qiita::Client.new(access_token: ENV["RUBOTY_QIITA_TOKEN"])
+        qids.each do |qid, qtitle|
+          result = client.stock_item qid
+          logger.info "Qiita API: status=#{result.status} body=#{result.body}" if $DEBUG
+        end
+        msg = "【P2Q】"
+        case qids.count
+        when 0
+          msg += "ストック対象はありません"
+        when 1
+          msg += "「#{qids.first[1]}」をストックしました"
+        else
+          msg += "「#{qids.first[1]}」他、#{qids.count}個の記事をストックしました"
+        end
+        return msg
+      end
+
+    end
+  end
+end
+
